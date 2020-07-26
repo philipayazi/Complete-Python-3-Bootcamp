98932ee814b3715e9b4755be1d1d8d2c44b0a7c3
Sourcery AI <>
2020-07-26 00:52:50 +0000

    'Refactored by Sourcery'

--- 17-Parallel Processing/test.py
+++ 17-Parallel Processing/test.py
@@ -8,14 +8,13 @@ def find_pi(n):
     """
     inside=0
 
-    for i in range(0,n):
+    for _ in range(n):
         x=random()
         y=random()
         if (x*x+y*y)**(0.5)<=1:  # if i falls inside the circle
             inside+=1
 
-    pi=4*inside/n
-    return pi
+    return 4*inside/n
 
 if __name__ == '__main__':
     N = 10**7  # total iterations
